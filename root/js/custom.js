bcfadfdcea518a6f2e8033ae9c0cfc54c4272170
Zach <zab5957@rit.edu>
2020-09-01 16:12:41 -0400

    portfolio updates homepage

--- root/js/custom.js
+++ root/js/custom.js
@@ -9,6 +9,66 @@
      *  SMOTH SCROOL JS
      * ----------------------------------------------------------------------------------------
      */
+    
+    $(document).ready(function() {
+
+	var getMax = function() {
+		return $(document).height() - $(window).height();
+	}
+
+	var getValue = function() {
+		return $(window).scrollTop();
+	}
+
+	if ('max' in document.createElement('progress')) {
+		var progressBar = $('progress');
+		
+		progressBar.attr({
+			max: getMax()
+		});
+
+		$(document).on('scroll', function() {
+			progressBar.attr({
+				value: getValue()
+			});
+		});
+
+		$(window).resize(function() {
+			
+			progressBar.attr({
+				max: getMax(),
+				value: getValue()
+			});
+		});
+
+	} else {
+
+		var progressBar = $('.progress-bar'),
+			max = getMax(),
+			value, width;
+
+		var getWidth = function() {
+			
+			value = getValue();
+			width = (value / max) * 100;
+			width = width + '%';
+			return width;
+		}
+
+		var setWidth = function() {
+			progressBar.css({
+				width: getWidth()
+			});
+		}
+
+		$(document).on('scroll', setWidth);
+		$(window).on('resize', function() {
+			
+			max = getMax();
+			setWidth();
+		});
+	}
+});
 
     $('a.smoth-scroll').on('click', function (e) {
         var anchor = $(this);
@@ -56,7 +116,67 @@
 
     });
 
+    /* ----------------------------------------------------------- */
+    /*  Typing effect homepage
+    /* ----------------------------------------------------------- */
+    
+    var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
 
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        document.body.appendChild(css);
+    };
+    
     /* ==========================================================================
       SCROLL SPY
  ========================================================================== */
