e372a411303ef6c6d035c771b62a4360872f3da3
Zach <zab5957@rit.edu>
2020-09-10 22:51:14 -0400

    test commits

--- root/src/utils/getPosts.js
+++
@@ -1,8 +0,0 @@
-import { POST } from '~/constants';
-
-const getPosts = data =>
-  data.posts.edges.filter(({ node: { frontmatter: { hide, type } } }) => (
-    hide !== true && (type || POST) === POST
-  ));
-
-export default getPosts;
